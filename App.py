e2996d8d40a677f69c4e565f04c78aaec5fa95d0
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-03-29 15:11:22 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+import os
+
+st.set_page_config(layout="wide")
+
+st.title(" PragyanAI Student Data Advanced Visualization App")
+
+# -----------------------------
+# SAFE DATA LOADING
+# -----------------------------
+uploaded_file = st.file_uploader("Upload CSV (Recommended)", type=["csv"])
+
+@st.cache_data
+def load_data(path):
+    return pd.read_csv(path)
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+else:
+    default_path = "student_PRICING_SCHOLARSHIP_Analysis_Project_12.csv"
+    
+    if os.path.exists(default_path):
+        df = load_data(default_path)
+    else:
+        st.error("❌ Dataset not found. Please upload CSV.")
+        st.stop()
+
+st.success("✅ Data Loaded Successfully")
